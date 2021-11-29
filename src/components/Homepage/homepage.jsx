be2f049412ef990fc993ac67268f7bf21a736189
Nishtha Sharma <nishthas1999@gmail.com>
2021-11-29 11:55:29 +0530

    Added Homepage

---
+++ src/components/Homepage/homepage.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import "./homepage.scss";
+
+const HomePage = () => {
+	return (
+		<>
+			{/* // New Item */}
+			<div className="homepage">
+				<div className="dir-menu">
+					<div className="menu-item">
+						<div className="content">
+							<h1 className="title">Hats</h1>
+							<span className="subtitle">Shop Now !</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* // New Item */}
+			<div className="homepage">
+				<div className="dir-menu">
+					<div className="menu-item">
+						<div className="content">
+							<h1 className="title">Hats</h1>
+							<span className="subtitle">Shop Now !</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* // New Item */}
+			<div className="homepage">
+				<div className="dir-menu">
+					<div className="menu-item">
+						<div className="content">
+							<h1 className="title">Hats</h1>
+							<span className="subtitle">Shop Now !</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* // New Item */}
+			<div className="homepage">
+				<div className="dir-menu">
+					<div className="menu-item">
+						<div className="content">
+							<h1 className="title">Hats</h1>
+							<span className="subtitle">Shop Now !</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* // New Item */}
+			<div className="homepage">
+				<div className="dir-menu">
+					<div className="menu-item">
+						<div className="content">
+							<h1 className="title">Hats</h1>
+							<span className="subtitle">Shop Now !</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default HomePage;
